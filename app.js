d98c266bb4d4ecd8ba27c518740b4b925acc3109
keshriabhi <abhishekkeshri@live.com>
2021-09-06 23:04:18 +0530

    commit

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const request = require('request');
 
-request('http://api.weatherstack.com/current?access_key=91e0f748cd2513dedd9adb536727d0fd&query=New%20York', (error, response, body)=>{
-    console.error('error : ',error);
-    console.log('Status Code : ',response && response.statusCode);
-    console.log('Body : ',body);
+const URL = 'http://api.weatherstack.com/current?access_key=91e0f748cd2513dedd9adb536727d0fd&query=New%20York';
+
+request({url : URL, json : true}, (error, response, body)=>{
+    // console.error('error : ',error);
+    // console.log('Status Code : ',response && response.statusCode);
+    // console.log('Body : ',body);
+    console.log(body.current.temperature);
 })
